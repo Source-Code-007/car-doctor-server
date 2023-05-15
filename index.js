bf8e74eae93d064d33b4affc32b2697bf7afb34f
Source-Code-007 <utsho926@gmail.com>
2023-05-15 11:24:31 +0600

    initial setup complete

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
 
     const carDoctorDB = client.db('car-doctor-DB')
     const servicesCollection = carDoctorDB.collection('services-collection')
